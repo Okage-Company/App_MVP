0f75339bcd3e628f83b8fb3caf3c774234389137
VinoliaBarreto <vinolia.barreto@gmail.com>
2021-08-21 16:39:56 +0000

    Almost there, looking for the right icons

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,14 +3,28 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<div className="big">
-			<div>1</div>
-			<div className="right">
-				<p>
-					2021@ <a href="http://okage.es/termandconditions">Terms & Conditions</a>-
-					<a href="http://okage.es/privacypolicy">Privacy Policy</a>-
-					<a href="http://okage.es/cookies">Cookies</a>
-				</p>
+			<div>
+				<div className="column left">OKAGE</div>
+				<div className="column about">
+					<div>
+						<a href="http://okage.es/aboutus">About</a>
+					</div>
+					<div>
+						<a href="http://okage.es/contactus">Contact us</a>
+					</div>
+				</div>
+				<div className="column middle" />
+				<div className="column iconos">
+					<i className="fa fa-facebook-official" aria-hidden="true" />
+					Facebook
+				</div>
 			</div>
+		</div>
+		<div className="direcciones">
+			<p>
+				2021@ <a href="http://okage.es/termandconditions">Terms & Conditions</a>-
+				<a href="http://okage.es/privacypolicy">Privacy Policy</a>-<a href="http://okage.es/cookies">Cookies</a>
+			</p>
 		</div>
 	</footer>
 );
