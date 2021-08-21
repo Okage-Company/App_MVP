84263e578d5b148f41bee19966fe667bd350e913
ricardorsario <ricardorsario@gmail.com>
2021-08-21 20:58:44 +0000

    card component, fonts added

---
+++ src/front/js/component/card.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "/workspace/react-flask-hello/src/front/styles/card.scss";
+
+const Card = props => {
+	return (
+		<div className="card_container">
+			<div className="card_image" />
+			<div className="card_container_text">
+				<span className="card_category">{props.category}</span>
+				<span className="card_title">{props.title}</span>
+				<span className="card_profile">{props.profile}</span>
+				<div className="card_container_footer">
+					<span className="card_address">{props.address}</span>
+					{props.icon}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+Card.propTypes = {
+	size: PropTypes.string,
+	image: PropTypes.string,
+	category: PropTypes.string,
+	title: PropTypes.string,
+	profile: PropTypes.string,
+	address: PropTypes.string,
+	icon: PropTypes.string
+};
+
+export default Card;
