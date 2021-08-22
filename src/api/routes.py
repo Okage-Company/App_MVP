b937af75d9cdedda1fae2d45ac7fbdde2fcfd0c1
9Shuck <rubb.phz@gmail.com>
2021-08-22 15:36:11 +0000

    estructura base de datos_1

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,18 +1,29 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+from datetime import timedelta
+
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from sqlalchemy import exc
+from werkzeug.security import check_password_hash
+
+from api.models import db, Account, Client, Business, Favourites, Services, Reviews, Comments
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
+@api.route('/account', methods=['GET, POST'])
+def get_account():
+    response_body = {
+        "message": "Hello! I'm the account"
+    }
+    return jsonify(response_body), 200
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+
+@api.route('/business', methods=['GET'])
+def get_business():
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend"
+        "message": "Hello! I'm the business"
     }
-
     return jsonify(response_body), 200
