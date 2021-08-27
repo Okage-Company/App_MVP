ae197386e58a23ae90d8ab95ca985607cc43120f
ricardorsario <ricardorsario@gmail.com>
2021-08-27 14:03:42 +0000

    moving my code to vinos footer

--- src/front/js/component/footer.jsx
+++ src/front/js/component/footer.jsx
@@ -1,31 +1,21 @@
 import React, { Component } from "react";
 
 const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<div className="big">
-			<div>
-				<div className="column left">OKAGE</div>
-				<div className="column about">
-					<div>
-						<a href="http://okage.es/aboutus">About</a>
-					</div>
-					<div>
-						<a href="http://okage.es/contactus">Contact us</a>
-					</div>
-				</div>
-				<div className="column middle" />
-				<div className="column iconos">
-					<i className="fa fa-facebook-official" aria-hidden="true" />
-					Facebook
-				</div>
-			</div>
+	<div className="footer_container">
+		<div className="footer_logo">
+			<img className="footer_logo_img" src="https://i.ibb.co/F82sCf8/0000-07.png"></img>
 		</div>
-		<div className="direcciones">
-			<p>
-				2021@ <a href="http://okage.es/termandconditions">Terms & Conditions</a>-
-				<a href="http://okage.es/privacypolicy">Privacy Policy</a>-<a href="http://okage.es/cookies">Cookies</a>
-			</p>
+		<div>
+			<div className="footer_text">About us</div>
+			<div>Contact</div>
 		</div>
-	</footer>
+		<div className="footer_social">
+			<i className="fab fa-instagram"></i>
+			<i className="fab sm-icon fa-facebook-square"></i>
+			<i className="fab sm-icon fa-twitter-square"></i>
+		</div>
+		<div className="footer_terms"></div>
+		<div className="footer_terms">2021© Terms & Conditions • Privacy Policy • Cookies</div>
+	</div>
 );
 export default Footer;
