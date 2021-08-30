1bb3599d1370f071add1c892a4f1a6145276c7f4
ricardorsario <ricardorsario@gmail.com>
2021-08-30 13:54:55 +0000

    i'm making the modal

---
+++ src/front/js/component/modal.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../styles/modal.scss";
+
+const Modal = () => {
+	return (
+		<div className="modal_container">
+			<div className="modal_content">
+				<button>CLOSE THIS BITCH</button>
+				<span>Hello boys</span>
+				<button>Click</button>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,16 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.scss";
 
 import Card from "/workspace/react-flask-hello/src/front/js/component/card.jsx";
-
+import Modal from "/workspace/react-flask-hello/src/front/js/component/modal.jsx";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+
+	const [openModal, setOpenModal] = useState(false);
 
 	return (
 		<div>
 			{/* card_search_view */}
 			<Card />
+			<button
+				className="open_modal_button"
+				onClick={() => {
+					setOpenModal(true);
+				}}>
+				Eat my shorts
+			</button>
+			{openModal && <Modal />}
 		</div>
 	);
 };
