7219a138d0fe49ba692d668a015788ff5c1d15d1
9Shuck <rubb.phz@gmail.com>
2021-08-30 19:52:26 +0000

    upload image button+vista business profile a falta de mapear y conectar el back

--- src/front/js/pages/businessProfile.jsx
+++ src/front/js/pages/businessProfile.jsx
@@ -1,16 +1,28 @@
 import React, { useContext } from "react";
-import "../../styles/business_profile.scss";
+import "../../styles/businessProfile.scss";
 import { Context } from "../store/appContext";
+import ButtonUploadImage from "../component/buttonUploadImage.jsx";
 
 const BusinessProfile = () => {
 	const { store } = useContext(Context);
 
 	return (
 		<div className="businessProfileContainer">
-			<div className="businessProfileSidebar">Hello</div>
+			<div className="businessProfileSidebar">
+				<div className="businessProfileImage"></div>
+				<div className="buttonProfileUpload">
+					<ButtonUploadImage />
+				</div>
+			</div>
 			<div className="businessProfileMain">
-				<h1>Businesses</h1>
-				<div>Title</div>
+				<div className="businessProfileTitle">Your Profile</div>
+				<div className="dataContainer">
+					<div>
+						<div className="dataContainerTitle">Name</div>
+						<div>Fernando</div>
+					</div>
+					<div className="dataContainerEdit">Edit</div>
+				</div>
 			</div>
 		</div>
 	);
