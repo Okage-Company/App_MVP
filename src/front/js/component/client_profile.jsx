563908ed943de2a2f1bec33c69f0f5b151eb4e82
VinoliaBarreto <vinolia.barreto@gmail.com>
2021-08-31 09:19:19 +0000

    Saving firs version

--- src/front/js/component/client_profile.jsx
+++ src/front/js/component/client_profile.jsx
@@ -9,24 +9,89 @@ const Client_Profile = () => {
 			{store.account.map((account, index) => {
 				return (
 					<div className="clientProfileContainer" key={index}>
-						<div className="clientProfileSidebar">hello</div>
-						<div className="clientProfileMain">
-							<div>Client Profile</div>
-							<div>
-								<div>
-									<p>User ID: {account.id}</p>
-								</div>
-								<div>
-									<p>Name: {account.name}</p>{" "}
-								</div>
-								<div>
-									<p>Last Name: {account.last_name}</p>{" "}
-								</div>
-								<div>
-									<p>Phone: {account.phone}</p>{" "}
-								</div>
-								<div>{account.address}</div>
+						<div className="clientProfileSidebar">
+							<div className="clientProfileSidebar">
+								<div className="clientProfileImage"></div>
 							</div>
+						</div>
+						<div className="clientProfileMain">
+							<div>
+								<div className="clientProfileTitle">Your Profile</div>
+								<div className="dataContainer">
+									<div>
+										<div className="dataContainerTitle">Name</div>
+										<div>{account.name}</div>
+									</div>
+									<div className="dataContainerEdit">Edit</div>
+								</div>
+								<div className="dataContainer">
+									<div>
+										<div className="dataContainerTitle">Last Name</div>
+										<div>{account.last_name}</div>
+									</div>
+									<div className="dataContainerEdit">Edit</div>
+								</div>
+								<div className="dataContainer">
+									<div>
+										<div className="dataContainerTitle">Email</div>
+										<div>{account.email}</div>
+									</div>
+									<div className="dataContainerEdit">Edit</div>
+								</div>
+							</div>
+							<div>
+								<div className="dataContainer">
+									<div>
+										<div className="dataContainerTitle">Province</div>
+										<div>{account.province}</div>
+									</div>
+									<div className="dataContainerEdit">Edit</div>
+								</div>
+								<div>
+									<div className="dataContainer">
+										<div>
+											<div className="dataContainerTitle">Post Code</div>
+											<div>{account.post_code}</div>
+										</div>
+										<div className="dataContainerEdit">Edit</div>
+									</div>
+								</div>
+								<div>
+									<div className="dataContainer">
+										<div>
+											<div className="dataContainerTitle">Address</div>
+											<div>{account.adress}</div>
+										</div>
+										<div className="dataContainerEdit">Edit</div>
+									</div>
+								</div>
+								<div>
+									<div className="dataContainer">
+										<div>
+											<div className="dataContainerTitle">Phone</div>
+											<div>{account.phone}</div>
+										</div>
+										<div className="dataContainerEdit">Edit</div>
+									</div>
+								</div>
+							</div>
+							<div>Password Update</div>
+							<div>
+								<div className="dataContainer">
+									<div>
+										<div className="dataContainerTitle">Current Password</div>
+										<div>{account._password}</div>
+									</div>
+									<div className="dataContainerEdit">Edit</div>
+								</div>
+							</div>
+							<div>
+								<p>New Password {account._new_password}</p>{" "}
+							</div>
+							<div>
+								<p>Confirm New Password: {account._new_password}</p>{" "}
+							</div>
+							<button>Update</button>
 						</div>
 					</div>
 				);
