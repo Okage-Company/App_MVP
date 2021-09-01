1489641617475c6086af9fed0ce10f0fc82c4017
Florence-Paloma <f.palomagaspar@gmail.com>
2021-09-01 13:01:00 +0000

    repaso componente botón

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.scss";
+import Delete from "../component/delete.jsx";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
-	return <div />;
+	return <Delete />;
 };
