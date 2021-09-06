2997c8e9ad3f076f31b958438d3e46d314848d96
Florence-Paloma <f.palomagaspar@gmail.com>
2021-09-06 02:28:05 +0000

    css mypage

--- src/front/js/pages/mypage.jsx
+++ src/front/js/pages/mypage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../../styles/mypage.scss";
 
 const Mypage = () => {
-	return <div className="mt-10">THIS IS MY PAGE</div>;
+	return (
+		<div className="mt-10">
+			<h4>THIS IS MY PAGE</h4>
+		</div>
+	);
 };
 
 export default Mypage;
