705014b129d0769b6abbc5ab4e6c043bda894a83
9Shuck <rubb.phz@gmail.com>
2021-09-16 10:40:21 +0000

    u.u

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -16,7 +16,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			clientId: [],
 			favouritesId: [],
 			businessId: [],
-			BASE_URL: "https://www.okage.es/",
+			BASE_URL: "https://okage.es/",
 			URL_API: "https://3001-copper-cougar-airoiadp.ws-eu16.gitpod.io/api/",
 			user: {},
 			currentUser: {}
